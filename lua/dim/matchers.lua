20e5888e631e7fdda0b3fd868e886267270b4d67
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-07-27 22:44:22 +0200

    feat: add support for private fields

--- lua/dim/matchers.lua
+++ lua/dim/matchers.lua
@@ -3,4 +3,5 @@ return {
   "unused", -- sumneko_lua rust_analyzer
   "536870973", -- jdtls variables
   "603979894", -- jdtls private methods
+  "570425421", -- jdtls private fields
 }
---
+++ tests/unused.java
@@ -0,0 +1,13 @@
+package tests;
+
+/** unused */
+class Unused {
+
+    private String unusedField = "dsadsa";
+
+    private void unusedMethod() {}
+
+    public static void main() {
+        String unusedVariable = "dsadasdas";
+    }
+}
